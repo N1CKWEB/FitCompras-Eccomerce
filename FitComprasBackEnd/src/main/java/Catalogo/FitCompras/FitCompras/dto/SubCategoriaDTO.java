0af9ac73a4acc25ba981a55542c59e0887b19976
lautaro321 <skoollarris9@gmail.com>
2025-02-11 19:06:02 -0300

    solucionando-problema-de-ramas

--- FitComprasBackEnd/src/main/java/Catalogo/FitCompras/FitCompras/dto/SubCategoriaDTO.java
+++ FitComprasBackEnd/src/main/java/Catalogo/FitCompras/FitCompras/dto/SubCategoriaDTO.java
@@ -1,25 +1,12 @@
 package Catalogo.FitCompras.FitCompras.dto;
-<<<<<<< HEAD
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-=======
-
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
->>>>>>> 1c559965fbcbd5136696eecbeb71d8b0f019fbfb
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SubCategoriaDTO {
     private Long id;
     private String nombre;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 1c559965fbcbd5136696eecbeb71d8b0f019fbfb
 }
