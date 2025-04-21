2b0adcb6fc2fa7694d2c9c40bdab87c35936521d
FrancoVillarLaz <135402258+FrancoVillarLaz@users.noreply.github.com>
2025-04-21 09:04:07 -0300

    update login response and user service

--- FitComprasBackEnd/src/main/java/Catalogo/FitCompras/FitCompras/service/UserService.java
+++ FitComprasBackEnd/src/main/java/Catalogo/FitCompras/FitCompras/service/UserService.java
@@ -1,9 +1,13 @@
 package Catalogo.FitCompras.FitCompras.service;
 
+import Catalogo.FitCompras.FitCompras.dto.LoginResponse;
 import Catalogo.FitCompras.FitCompras.entities.User;
 import Catalogo.FitCompras.FitCompras.repositories.UserRepository;
+import Catalogo.FitCompras.FitCompras.util.LoginRequest;
 import Catalogo.FitCompras.FitCompras.util.RegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,4 +30,19 @@ public class UserService {
         
         userRepository.save(user);
     }
+    public LoginResponse login(LoginRequest request) {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Contraseña incorrecta");
+        }
+
+        return new LoginResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getRoles()
+        );
+        }
+
 }
